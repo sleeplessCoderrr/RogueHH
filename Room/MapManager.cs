ee431b55f580827395de4efa749b6a587f5f0b9d
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-11-22 16:31:57 +0700

    fixing lag issue but missing the AStar

--- Room/MapManager.cs
+++ Room/MapManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
+    [Header("Prefabs for Floor")]
     public GameObject floorTile;
     private Tile[,] _mapGrid;
-    private List<Vector2Int> _roomCenters = new List<Vector2Int>();
     private List<Room> _rooms;
     private MapBuilder _mapBuilder;
-
+    private List<Vector2Int> _roomCenters = new List<Vector2Int>();
+    private KruskalMST _kruskalMst = new KruskalMST();
+    
     private void Start()
     {
         _mapBuilder = new MapBuilder();
@@ -25,14 +27,18 @@ public class MapManager : MonoBehaviour
         _roomCenters = GetAllRoomCenters();
 
         //A* and MST pathfinding
-        var mstEdges = ComputeKruskalMst(_roomCenters);
+        var mstEdges = _kruskalMst.Compute(_roomCenters);
         foreach (var edge in mstEdges)
         {
             _mapBuilder.AddTunnelBetweenPoints(edge.Item1, edge.Item2);
-            var path = AStarPathfinder.FindPath(_mapGrid, edge.Item1, edge.Item2);
-            CreateTunnel(path);
+            
+            //To be Search Again
+            //===================
+            // var path = AStarPathfinder.FindPath(_mapGrid, edge.Item1, edge.Item2);
+            // CreateTunnel(path);
         }
-        Debug.Log("Ended");
+        _mapBuilder.Build();
+       
     }
     
     private List<Vector2Int> GetAllRoomCenters()
@@ -46,83 +52,14 @@ public class MapManager : MonoBehaviour
 
         return roomCenters;
     }
-    
-    private List<(Vector2Int, Vector2Int)> ComputeKruskalMst(List<Vector2Int> roomCenters)
-    {
-        var mstEdges = new List<(Vector2Int, Vector2Int)>();
-        var edges = new List<(float, Vector2Int, Vector2Int)>();
-
-        // Generate all edges with distances
-        for (int i = 0; i < roomCenters.Count; i++)
-        {
-            for (int j = i + 1; j < roomCenters.Count; j++)
-            {
-                float distance = Vector2Int.Distance(roomCenters[i], roomCenters[j]);
-                edges.Add((distance, roomCenters[i], roomCenters[j]));
-            }
-        }
-
-        // Sort edges by distance
-        edges.Sort((a, b) => a.Item1.CompareTo(b.Item1));
-
-        // Initialize Union-Find structure
-        var parent = new Dictionary<Vector2Int, Vector2Int>();
-        foreach (var room in roomCenters)
-        {
-            parent[room] = room; // Each room starts as its own parent
-        }
-
-        // Find root of a set (with path compression)
-        Vector2Int Find(Vector2Int node)
-        {
-            if (parent[node] != node)
-            {
-                parent[node] = Find(parent[node]);
-            }
-            return parent[node];
-        }
-
-        // Union two sets
-        void Union(Vector2Int a, Vector2Int b)
-        {
-            var rootA = Find(a);
-            var rootB = Find(b);
-            if (rootA != rootB)
-            {
-                parent[rootB] = rootA;
-            }
-        }
-
-        // Add edges to the MST
-        foreach (var (distance, room1, room2) in edges)
-        {
-            if (Find(room1) != Find(room2)) // Avoid cycles
-            {
-                mstEdges.Add((room1, room2));
-                Union(room1, room2);
-
-                // Stop if we have enough edges for an MST
-            }
-            if (mstEdges.Count == roomCenters.Count - 1)
-            {
-                    break;
-            }
-            }
-
-        return mstEdges;
-    }
 
     private void CreateTunnel(List<Vector2Int> path)
     {
         foreach (var point in path)
         {
-            // Ensure that the tile at this point is walkable
             _mapGrid[point.x, point.y].IsWalkable = true;
             _mapBuilder.Build();
-            // Optionally, you can add other actions, like marking the tile with specific tunnel graphics
-            // or other features if you wish to distinguish tunnels from regular walkable tiles.
         }
-        
     }
 }
 
---
+++ Room/algorithms/KruskalMST.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class KruskalMST
+{
+    private Dictionary<Vector2Int, Vector2Int> _parent;
+
+    public KruskalMST()
+    {
+        _parent = new Dictionary<Vector2Int, Vector2Int>();
+    }
+
+    public List<(Vector2Int, Vector2Int)> Compute(List<Vector2Int> roomCenters)
+    {
+        var mstEdges = new List<(Vector2Int, Vector2Int)>();
+        var edges = new List<(float, Vector2Int, Vector2Int)>();
+
+        for (var i = 0; i < roomCenters.Count; i++)
+        {
+            for (var j = i + 1; j < roomCenters.Count; j++)
+            {
+                var distance = Vector2Int.Distance(roomCenters[i], roomCenters[j]);
+                edges.Add((distance, roomCenters[i], roomCenters[j]));
+            }
+        }
+
+        edges.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+        foreach (var room in roomCenters)
+        {
+            _parent[room] = room; // Each room starts as its own parent
+        }
+
+        foreach (var (distance, room1, room2) in edges)
+        {
+            if (Find(room1) != Find(room2)) // Avoid cycles
+            {
+                mstEdges.Add((room1, room2));
+                Union(room1, room2);
+            }
+
+            if (mstEdges.Count == roomCenters.Count - 1)
+            {
+                break;
+            }
+        }
+
+        return mstEdges;
+    }
+
+    private Vector2Int Find(Vector2Int node)
+    {
+        if (_parent[node] != node)
+        {
+            _parent[node] = Find(_parent[node]);
+        }
+        return _parent[node];
+    }
+
+    private void Union(Vector2Int a, Vector2Int b)
+    {
+        var rootA = Find(a);
+        var rootB = Find(b);
+        if (rootA != rootB)
+        {
+            _parent[rootB] = rootA;
+        }
+    }
+}
