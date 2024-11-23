658bd440ff53d05a6fa0512a2a726167386c944f
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-11-23 18:01:24 +0700

    finish random spawning player

--- Player/Builder/PlayerBuilder.cs
+++ Player/Builder/PlayerBuilder.cs
@@ -2,5 +2,38 @@
 
 public class PlayerBuilder
 {
+    [Header("Data")]
+    private PlayerData _playerData;
+    private PlayerConfig _playerConfig;
     
+    public PlayerBuilder SetData(PlayerConfig playerConfig, PlayerData playerData)
+    {
+        _playerConfig = playerConfig;
+        _playerData = playerData;
+        return this;
+    }
+
+    private bool IsValidPosition(MapData mapData, int x, int y)
+    {
+        return mapData.MapTileData[x, y].IsRoom;
+    }
+
+    public PlayerBuilder InitializeRandomPosition(MapConfig mapConfig, MapData mapData)
+    {
+        var isNotValid = false;
+        while (!isNotValid)
+        {
+            var x = Random.Range(0, mapConfig.width - 1);
+            var y = Random.Range(0, mapConfig.height - 1);
+            if (IsValidPosition(mapData, x, y))
+            {
+                    var worldPosition = new Vector3(x*2, 1, y*2);
+                    GameObject.Instantiate(_playerConfig.playerPrefab, worldPosition, Quaternion.identity);
+                    _playerData.playerPosition = new Vector2(x, y);
+                    isNotValid = true;
+            }
+        }
+        
+        return this;
+    }
 }
--- Player/PlayerStateManager.cs
+++ Player/PlayerStateManager.cs
@@ -1,7 +1,52 @@
-﻿using States;
+﻿using System;
+using System.Threading.Tasks;
+using States;
 using UnityEngine;
 
-public class PlayerStateManager 
+public enum PlayerState
 {
+    Idle,
+    Walking
+}
+
+public class PlayerStateManager : MonoBehaviour
+{
+    [Header("Player Config & Data")]
+    public PlayerConfig playerConfig;
+    public PlayerData playerData;
+    private Player _player;
     
+    [Header("Importing Map Data")]
+    public MapData mapData;
+    public MapConfig mapConfig;
+
+    private PlayerBuilder _playerBuilder;
+
+    private void Awake()
+    {
+        _player = new Player(playerConfig, playerData);
+        _playerBuilder = new PlayerBuilder();
+    }
+
+    private async void Start()
+    {
+        //Delay 3 Seconds
+        await Task.Delay(3000);
+        
+        GetMapData();
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        _playerBuilder
+            .SetData(_player.PlayerConfig, _player.PlayerData)
+            .InitializeRandomPosition(mapConfig, mapData);
+    }
+
+    private void GetMapData()
+    {
+        mapData = MapManager.Instance.mapData;
+        mapConfig = MapManager.Instance.mapConfig;
+    }
 }
