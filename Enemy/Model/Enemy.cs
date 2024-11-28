3110f3b65433fe0f20c008913a9073c6a27affea
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-11-28 17:06:31 +0700

    making enemy model

---
+++ Enemy/Model/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Enemy
+{
+    [Header("Enemy Config")]
+    public EnemyConfig EnemyConfig;
+    
+    [Header("Enemy Data")]
+    public EnemyData EnemyData;
+
+    public Enemy(EnemyConfig enemyConfig, EnemyData enemyData)
+    {
+        this.EnemyConfig = enemyConfig;
+        this.EnemyData = enemyData;
+    }
+}
